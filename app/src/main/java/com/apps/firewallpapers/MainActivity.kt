712c73d7e99222f3c96992eb71c094f37678f9f7
Abhinav <abhi2000.rtk@gmail.com>
2020-05-21 22:32:10 +0530

    Initial commit

---
+++ app/src/main/java/com/apps/firewallpapers/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.apps.firewallpapers
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

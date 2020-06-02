416b15656fcb4dc462b2f0cca60abcd65e56fa8b
Abhinav <abhi2000.rtk@gmail.com>
2020-06-02 16:30:09 +0530

    Added night mode

--- app/src/main/java/com/apps/firewallpapers/HomeFragment.kt
+++ app/src/main/java/com/apps/firewallpapers/HomeFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment() {
 
@@ -14,6 +16,16 @@ class HomeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //Initializing action bar
+        (activity as AppCompatActivity).setSupportActionBar(main_toolbar)
+
+        val actionBar = (activity as AppCompatActivity).supportActionBar
+        actionBar!!.title = "Fire Wallpapers"
     }
 
 }
